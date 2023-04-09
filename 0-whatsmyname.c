099f566e5af5a6e7c0c840e097c559a721fd9396
AsmaaAliBahgat <asmaabhgat6@gmail.com>
2023-04-09 12:01:00 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ *
+ * Return: EXIT_SUCCESS
+ * aUTHOR: IanoNjuguna
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+
+	exit(EXIT_SUCCESS);
+}
